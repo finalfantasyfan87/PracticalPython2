1ea08abeca389dbe46b2ef541e9c38efd873a804
Jen <jenster8705@gmail.com>
2025-07-31 20:09:18 -0500

    Create scrapeWebRefactored.py
    Create Screenshot 2025-07-31 at 8.09.43 PM.png

---
+++ scrapeWebRefactored.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+#this method takes in a url of your choosing and the html tag you want to parse
+def scrape_it(url, tag_name):
+    #Uses the requests library to send an HTTP GET request to the specified URL.
+    #The response object contains the webpage’s content, status code, headers, etc.
+    response = requests.get(url)
+    #Passes the response’s text (HTML content) to BeautifulSoup, which parses the HTML into a navigable structure.
+    soup = BeautifulSoup(response.content, 'html.parser')
+    #here we tell our instance of BeautifulSoup to find all html elements
+    #this can be leveraged to find
+    tags = soup.find_all(tag_name)
+    for tag in tags:
+        #print the text of each span
+        print(tag.name, " " ,tag.text)
+
+#call my method and use it for victoria secret's website
+print(scrape_it('https://www.victoriassecret.com/us/','span'))
+print(scrape_it('https://www.victoriassecret.com/us/','h2'))
+
+
